d97f1a2e32a615adc087daf03f06cfbfcfc8923f
Oscar Olsson <happybits@gmail.com>
2022-09-27 16:21:52 +0200

    -

---
+++ Frontend/JavaScript/Components/Train/JavaScript/HintA.js
@@ -0,0 +1,20 @@
+const result = document.querySelector(".train .result")
+const from = document.querySelector(".train .from")
+const to = document.querySelector(".train .to")
+const okbutton = document.querySelector(".train button")
+
+update()
+
+function update() {
+    // todo: use this function to enable/disable buttons and show/hide stuff   
+    // hint: to disable an element, write: myElement.disabled = false
+    console.log("update")
+}
+
+function wayChange(way) {
+    console.log(`You want to change to "${way}"`)
+}
+
+function makeChoice() {
+    console.log("make choice")
+}
