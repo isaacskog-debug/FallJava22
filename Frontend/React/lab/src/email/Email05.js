a5c8890061e8149d5cb297c00561af1f403cd8f4
Oscar Olsson <happybits@gmail.com>
2022-09-30 14:13:50 +0200

    -

---
+++ Frontend/React/lab/src/email/Email05.js
@@ -0,0 +1,73 @@
+
+import { useState } from 'react'
+import './Email.css'
+
+function Email() {
+
+    const [input, setInput] = useState("")
+    const [inputAgain, setInputAgain] = useState("")
+
+    const inputChange = (event) => {
+        setInput(event.target.value)
+    }
+
+    const inputAgainChange = (event) => {
+        setInputAgain(event.target.value)
+    }
+    
+    const sendForm = () => {
+        if (input === ""){
+            console.log("Empty email address")
+            return
+        }
+
+        if (!isValidEmailAddress(input)) {
+            console.log("Invalid email address")
+            return
+        }
+
+        if (input !== inputAgain) {
+            console.log("Not the same")
+            return            
+        }
+
+        console.log("Success!")
+    }
+
+    return (
+
+        <article className="email">
+
+            <div className="group">
+                <label>Email</label>
+                <input className="inputEmail" type="email" value={input} onChange={inputChange} />
+            </div>
+
+            <div className="group">
+                <label>Enter again</label>
+                <input className="inputEmailAgain" type="email" value={inputAgain} onChange={inputAgainChange} />
+            </div>
+
+            <button onClick={sendForm}>Ok</button>
+
+            <div className="result hidden">
+                You entered ....
+            </div>
+
+        </article>
+
+    )
+}
+
+function isValidEmailAddress(email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+}
+
+
+export default Email
+
+
--- Frontend/React/lab/src/index.js
+++ Frontend/React/lab/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Email from './email/Email05'
+import Email from './email/Email06'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
