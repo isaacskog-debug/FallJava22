17d6e71230f4044defdd27d0ddf3d9ae6374f8dc
Oscar Olsson <happybits@gmail.com>
2022-09-30 08:51:45 +0200

    -

--- Frontend/React/lab/src/email/Email01.js
+++ Frontend/React/lab/src/email/Email01.js
@@ -1,4 +1,12 @@
 
+/*
+Copy the HTML-code from the exercise
+
+Change "class" to "className"
+
+Note that React is more senitive about the HTML-code (it has be be lowercase and you must close all tags)
+*/
+
 import './Email.css'
 function Email() {
 
