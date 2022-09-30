90cbc3c29814a3196c0433d2a9983cc754ff6242
Oscar Olsson <happybits@gmail.com>
2022-09-30 10:41:22 +0200

    -

--- Frontend/React/lab/src/index.js
+++ Frontend/React/lab/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import Email from './email/Email01'
+import Email from './email/Email02'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <h1>Hej</h1>
-    <h1>Hej</h1>
-    
     <Email />
-    <Email />
-
-    <h1>Hej</h1>
-
     <Email />
 
   </React.StrictMode>
